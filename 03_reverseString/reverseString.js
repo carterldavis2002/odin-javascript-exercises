e9751565113e4a42e2f0ff252776c04b9a4df37a
carterldavis2002 <carterldavis2002@gmail.com>
2022-05-17 16:17:22 -0500

    Exercise 3 documentation

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,13 @@
 const reverseString = function(string) {
-    let reverse = "";
+    let reverse = ""; //String to hold resulting reversed string
+
+    //Loop through every character in the passed string in reverse order
     for(let i = string.length - 1;i >= 0;i--)
     {
+        //Add the current character to the reverse string
         reverse += string.charAt(i);
     }
-    return reverse;
+    return reverse; //Return reversed result
 };
 
 // Do not edit below this line
