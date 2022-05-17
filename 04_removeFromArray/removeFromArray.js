48c3018f4a68d7b89f69b8f949b008db44716ae9
carterldavis2002 <carterldavis2002@gmail.com>
2022-05-17 16:12:52 -0500

    Added alternate solution and documentation

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,9 +2,8 @@
 NOTE: This implementation passes all tests but realistically,
 you would probably want a function that removes values that appear
 more than once which are specified in the argument list. This implementation
-only removes 1 occurrence and not all of them. An easy fix to this would be
-to declare an empty array and push elements from the passed array as long as they are not equal
-to the current argument value in the loop.
+only removes 1 occurrence and not all of them. Check the alternative solution
+below to see an example that removes duplicate values and still passes all the tests.
 */
 const removeFromArray = function() {
     let args = Array.from(arguments); //Makes an array out of the passed arguments
@@ -23,6 +22,33 @@ const removeFromArray = function() {
         }
     }
     return arr; //Return the array with values removed
+};
+
+const removeFromArrayAlternate = function() {
+    let args = Array.from(arguments); //Makes an array out of the passed arguments 
+    let arr = args[0]; //First argument is the array we want to remove values from
+    let newArr = []; //Empty array that will contain elements not present in args list
+    let found = false; //Has the current arg value been found in the array to remove from?
+
+    //Loop through array to remove elements from
+    for(let i = 0;i < arr.length;i++)
+    {
+        found = false; //Reset found
+
+        //Loop through args list for values to remove from array
+        for(let j = 1;j < args.length;j++)
+        {
+            //If the value to remove is found in the array, set found to true
+            if(arr[i] === args[j])
+                found = true;
+        }
+
+        //Don't add values to the array that have been found in arg values list
+        if(!found)
+            newArr.push(arr[i]);
+    }
+
+    return newArr; 
 };
 
 // Do not edit below this line
