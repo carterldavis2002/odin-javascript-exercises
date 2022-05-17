02095c4a28dcdec4cf58f7276654e414b14c9bfc
carterldavis2002 <carterldavis2002@gmail.com>
2022-05-17 15:16:23 -0500

    Completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  let repeated = ""; //Blank string to return if num == 0
 
+  //Return ERROR if num is negative
+  if(num < 0)
+    return "ERROR";
+
+  //Add string to repeated num times
+  for(let i = 0;i < num;i++)
+  {
+      repeated += string;
+  }
+  return repeated; //Return string repeated num times
 };
 
 // Do not edit below this line
