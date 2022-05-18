a75433f3d203325b55b7710bfd8ef647f4e738b3
carterldavis2002 <carterldavis2002@gmail.com>
2022-05-17 19:18:29 -0500

    Completed exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //Leap year if divisible by 100 and 400
+    if(year % 100 === 0 && year % 400 === 0)
+        return true;
+    
+    //Leap year if divisible by 4 and not 100
+    if(year % 100 !== 0 && year % 4 === 0)
+        return true;
 
+    return false; //Not leap year otherwise
 };
 
 // Do not edit below this line
