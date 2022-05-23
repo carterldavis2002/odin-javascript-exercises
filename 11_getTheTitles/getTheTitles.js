d50ba341e463cffd7c478c1c250dcd94b09860e3
carterldavis2002 <carterldavis2002@gmail.com>
2022-05-23 13:02:24 -0500

    Completed getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
-
+/*
+Mine and the official solution's implementations are identical.
+The important thing to remember about map() is that it returns an
+array that is the same size of the array calling the method. Since
+we want to return ALL the titles, we use map() to access and add the title 
+property of each object to the return array.
+*/
+const getTheTitles = function(titles) {
+    return titles.map((item) => item.title);
 };
 
 // Do not edit below this line
